881597d6b20787c24212cb451865521f7aeebee5
R.N <ngry@mcmaster.ca>
2014-08-20 01:08:21 -0400

    Added cachemean.R to understand the algorithm better.
    Coded up cachematrix.R and included comments as required

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())
+{
+    # This should store the inverse of the matrix we are interested in
+    minv <- NULL    
 
-    # This is a random statement to track a change.
-    
+    # Hard reset of the matrix inverse to allow us to recache the inverse
+    set <- function(y)   
+        {
+            x <<- y
+            minv <<- NULL
+        }
+
+    # Return the input matrix
+    get <- function() x     
+
+    # Set the inverse (without calculations). This is just an assignment operation
+    setinv <- function( matrixinv ) minv <<- matrixinv
+
+    # Return the inverse of the matrix
+    getinv <- function() minv
+
+    # return an object of functions
+    list(set = set, get = get,
+         setinv = setinv,
+          getinv = getinv)
     
 }
 
@@ -15,4 +36,39 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    # Just get the inverse (without calculating it explicitly)
+    minv <- x$getinv()
+    
+    # If the inverse is not null, it means that it has already been calculated
+    # therefore we can just retriee that value from cache
+    if(!is.null(minv))   
+        {
+            # print a message indicating it's cached and then return it
+            message("getting cached data")
+            return(minv)
+        }
+
+    # If we make it to this point it means the inverser has not been calculated
+    # therefore we need to calculate it.
+
+    # Get the matrix first
+    data <- x$get()
+
+    # Now solve for the inverse assuming it exists
+    minv <- solve(data, ...)
+
+    # Set this value in the Make Vector object, i.e. cacheing
+    x$setinv(minv)
+
+    #return the inverse
+    minv
+    
 }
+
+mat<- matrix( c(1,2,3,4), 2,2)
+
+mymat<-makeCacheMatrix(mat)
+
+cacheSolve(mymat)
+cacheSolve(mymat)
